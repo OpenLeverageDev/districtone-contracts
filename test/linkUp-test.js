a29c20ae2a29647cd0e30c3fda09c5ee0c613833
Jimmy <hello@openleverage.finance>
2023-12-16 14:31:25 +0800

    Add more tests

--- test/linkUp-test.js
+++ test/linkUp-test.js
@@ -33,8 +33,7 @@ describe("LinkUp Contract", function () {
 
             // Sign inviter address
             const inviter = addr2.address;
-            const addressBytes = hexStringToArray(ethers.keccak256(inviter));
-            let rawSig = owner.signMessage(addressBytes);
+            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
             expect(await linkUp.connect(addr1).verifySig(owner.address, inviter, rawSig)).to.be.true
             const joinTx = await linkUp.connect(addr1).join(inviter, rawSig, { value: ethers.parseEther("0.0015") });
 
@@ -43,37 +42,84 @@ describe("LinkUp Contract", function () {
             expect(await linkUp.inviterOf(invitee)).to.equal(inviter);
         });
 
-        // it("Should fail if JOIN_FEE is not met", async function () {
-        //     let rawSig = await owner.signMessage(addr2.address);
-        //     let sig = Signature.from(rawSig);
-        //     await expect(linkUp.connect(addr1).join(addr2.address, sig, { value: ethers.parseEther("0.001") }))
-        //         .to.be.revertedWith('Incorrect fee');
-        // });
+        it("Should fail if JOIN_FEE is not met", async function () {
+            const inviter = addr2.address;
+            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            await expect(linkUp.connect(addr1).join(inviter, rawSig, {value: ethers.parseEther("0.001")}))
+                .to.be.revertedWith('Incorrect fee');
+        });
 
-        // Add more tests for other scenarios, such as invalid inviter, already joined, etc.
+        it("Should fail if the inviter is himself", async function () {
+            const inviter = addr1.address;
+            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            await expect(linkUp.connect(addr1).join(inviter, rawSig, {value: ethers.parseEther("0.0015")}))
+                .to.be.revertedWith('Invalid inviter');
+        });
+
+        it("Should fail if the inviter is zero address", async function () {
+            const inviter = ethers.ZeroAddress;
+            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            await expect(linkUp.connect(addr1).join(inviter, rawSig, {value: ethers.parseEther("0.0015")}))
+                .to.be.revertedWith('Invalid inviter');
+        });
+
+        it("Should fail if the user tries to join twice", async function () {
+            const inviter = addr2.address;
+            let signature = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            await linkUp.connect(addr1).join(inviter, signature, {value: ethers.parseEther("0.0015")});
+            await expect(linkUp.connect(addr1).join(inviter, signature, {value: ethers.parseEther("0.0015")}))
+                .to.be.revertedWith('Already joined');
+        });
+
+
     });
 
     describe("Withdrawing balance", function () {
-        // Tests for the withdraw function
+        it("Should allow a user to withdraw their balance", async function () {
+            // Setup: addr1 joins under addr2, then addr3 joins under addr1
+            const signature1 = await owner.signMessage(hexStringToArray(ethers.keccak256(addr1.address)));
+            const signature2 = await owner.signMessage(hexStringToArray(ethers.keccak256(addr2.address)));
+            await linkUp.connect(addr1).join(addr2.address, signature2, {value: ethers.parseEther("0.0015")});
+            await linkUp.connect(addrs[3]).join(addr1.address, signature1, {value: ethers.parseEther("0.0015")});
+
+            // Withdraw balance
+            const initialBalance = await ethers.provider.getBalance(addr1.address);
+            const withdrawTx = await linkUp.connect(addr1).withdraw();
+            const receipt = await withdrawTx.wait();
+
+            // Using BigInt for arithmetic operation
+            let gasPrice;
+            if (receipt.effectiveGasPrice) {
+                gasPrice = BigInt(receipt.effectiveGasPrice.toString());
+            } else {
+                const tx = await ethers.provider.getTransaction(withdrawTx.hash);
+                gasPrice = tx.gasPrice ? BigInt(tx.gasPrice.toString()) : BigInt(tx.maxFeePerGas.toString());
+            }
+
+            const gasUsed = BigInt(receipt.cumulativeGasUsed) * gasPrice;
+            const finalBalance = await ethers.provider.getBalance(addr1.address);
+
+            expect(BigInt(finalBalance.toString()) + gasUsed).to.be.above(BigInt(initialBalance.toString()));
+        });
+
+        it("Should fail if there is no balance to withdraw", async function () {
+            await expect(linkUp.connect(addr1).withdraw())
+                .to.be.revertedWith('No balance to withdraw');
+        });
     });
 
-    // Add more test cases as needed
 });
 
 function hexStringToArray(hexString) {
     if (hexString.startsWith('0x')) {
         hexString = hexString.slice(2);
     }
-
     if (hexString.length % 2 !== 0) {
         hexString = '0' + hexString;
     }
-
     const byteArray = new Uint8Array(hexString.length / 2);
-
     for (let i = 0, j = 0; i < hexString.length; i += 2, j++) {
         byteArray[j] = parseInt(hexString.slice(i, i + 2), 16);
     }
-
     return byteArray;
 }
