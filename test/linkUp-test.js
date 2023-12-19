166c8df3e1fdcb72dfc6de3055b32400d611530b
Jimmy <hello@openleverage.finance>
2023-12-19 09:39:02 +0800

    Add signer address

--- test/linkUp-test.js
+++ test/linkUp-test.js
@@ -9,18 +9,18 @@ describe("LinkUp Contract", function () {
     let addr1;
     let addr2;
     let addrs;
-
+    let signer;
 
     beforeEach(async function () {
         LinkUp = await ethers.getContractFactory("LinkUp");
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-
-        linkUp = await LinkUp.connect(owner).deploy();
+        signer = addrs[9];
+        linkUp = await LinkUp.connect(owner).deploy(signer);
     });
 
     describe("Deployment", function () {
         it("Should set the right owner", async function () {
-            expect(await linkUp.rootAddress()).to.equal(owner.address);
+            expect(await linkUp.owner()).to.equal(owner.address);
         });
 
         it("Should set the JOIN_FEE correctly", async function () {
@@ -33,8 +33,7 @@ describe("LinkUp Contract", function () {
 
             // Sign inviter address
             const inviter = addr2.address;
-            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
-            expect(await linkUp.connect(addr1).verifySig(owner.address, inviter, rawSig)).to.be.true
+            let rawSig = signer.signMessage(hexStringToArray(ethers.keccak256(inviter)));
             const joinTx = await linkUp.connect(addr1).join(inviter, rawSig, { value: ethers.parseEther("0.0015") });
 
             let invitee = addr1.address;
@@ -44,28 +43,28 @@ describe("LinkUp Contract", function () {
 
         it("Should fail if JOIN_FEE is not met", async function () {
             const inviter = addr2.address;
-            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            let rawSig = signer.signMessage(hexStringToArray(ethers.keccak256(inviter)));
             await expect(linkUp.connect(addr1).join(inviter, rawSig, {value: ethers.parseEther("0.001")}))
                 .to.be.revertedWith('Incorrect fee');
         });
 
         it("Should fail if the inviter is himself", async function () {
             const inviter = addr1.address;
-            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            let rawSig = signer.signMessage(hexStringToArray(ethers.keccak256(inviter)));
             await expect(linkUp.connect(addr1).join(inviter, rawSig, {value: ethers.parseEther("0.0015")}))
                 .to.be.revertedWith('Invalid inviter');
         });
 
         it("Should fail if the inviter is zero address", async function () {
             const inviter = ethers.ZeroAddress;
-            let rawSig = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            let rawSig = signer.signMessage(hexStringToArray(ethers.keccak256(inviter)));
             await expect(linkUp.connect(addr1).join(inviter, rawSig, {value: ethers.parseEther("0.0015")}))
                 .to.be.revertedWith('Invalid inviter');
         });
 
         it("Should fail if the user tries to join twice", async function () {
             const inviter = addr2.address;
-            let signature = owner.signMessage(hexStringToArray(ethers.keccak256(inviter)));
+            let signature = signer.signMessage(hexStringToArray(ethers.keccak256(inviter)));
             await linkUp.connect(addr1).join(inviter, signature, {value: ethers.parseEther("0.0015")});
             await expect(linkUp.connect(addr1).join(inviter, signature, {value: ethers.parseEther("0.0015")}))
                 .to.be.revertedWith('Already joined');
@@ -77,8 +76,8 @@ describe("LinkUp Contract", function () {
     describe("Withdrawing balance", function () {
         it("Should allow a user to withdraw their balance", async function () {
             // Setup: addr1 joins under addr2, then addr3 joins under addr1
-            const signature1 = await owner.signMessage(hexStringToArray(ethers.keccak256(addr1.address)));
-            const signature2 = await owner.signMessage(hexStringToArray(ethers.keccak256(addr2.address)));
+            const signature1 = await signer.signMessage(hexStringToArray(ethers.keccak256(addr1.address)));
+            const signature2 = await signer.signMessage(hexStringToArray(ethers.keccak256(addr2.address)));
             await linkUp.connect(addr1).join(addr2.address, signature2, {value: ethers.parseEther("0.0015")});
             await linkUp.connect(addrs[3]).join(addr1.address, signature1, {value: ethers.parseEther("0.0015")});
 
